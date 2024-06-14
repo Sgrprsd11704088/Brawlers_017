abb92ab0a26a84d86b8292858f3f6e7992dad722
Sagar Prasad <sagar.11704088@gmail.com>
2024-06-14 13:41:44 +0530

    conflicts resolved + code refactored

--- backend/routes/DonorRoutes.js
+++ backend/routes/DonorRoutes.js
@@ -1,16 +1,18 @@
-
-import express from 'express';
-import Donation from '../models/DonorModel.js';
-
+import express from "express";
+import Donation from "../models/DonorModel.js";
 
 const Donorrouter = express.Router();
 
-Donorrouter.post('/create', async (req, res) => {
-  const { amount, donor, name , message } = req.body;
+Donorrouter.post("/create", async (req, res) => {
+  const { amount, donor, name, message } = req.body;
 
   // Validate the request body
-  if (!amount || !donor || !name ) {
-    return res.status(400).json({ message: 'Please include all required fields: amount, donor, and name' });
+  if (!amount || !donor || !name) {
+    return res
+      .status(400)
+      .json({
+        message: "Please include all required fields: amount, donor, and name",
+      });
   }
 
   try {
@@ -22,18 +24,16 @@ Donorrouter.post('/create', async (req, res) => {
   }
 });
 
-Donorrouter.get('/', async (req, res) => {
-    try {
-      const donations = await Donation.find().sort({ date: -1 }).populate('donor', ['name', 'email']);
-      res.json(donations);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
-  });
-
+Donorrouter.get("/", async (req, res) => {
+  try {
+    const donations = await Donation.find()
+      .sort({ date: -1 })
+      .populate("donor", ["name", "email"]);
+    res.json(donations);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 export default Donorrouter;
-
-
-
