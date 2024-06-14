2b44b2a8575cc1fe896edb24a0d2cbca5ffae7a5
Sagar Prasad <sagar.11704088@gmail.com>
2024-06-14 21:07:24 +0530

    Components for admin flow in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import Auth from "./components/login.jsx";
 import ProjectList from "./components/ProjectList";
 import projects from "./assets/exampleProjects.json";
 import CreateProject from "./components/CreateProject";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Admin/Home.jsx";
+import Login from "./components/Admin/Login.jsx";
 
 function App() {
   return (
     <>
-    <Auth></Auth>      
+      <Auth />
       <h1>Home Page</h1>
+      <CreateProject />
       <ProjectList projects={projects} />
-      <CreateProject/>
+
+      <Routes>
+        <Route path="/admin" element={<Home />}></Route>
+        <Route path="/admin/login" element={<Login />}></Route>
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/components/Admin/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Admin HomePage</h1>
+    </>
+  );
+};
+
+export default Home;
