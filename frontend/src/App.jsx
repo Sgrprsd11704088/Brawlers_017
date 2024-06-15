4eb5a754bfe529a92dbecd29717ae083c692af76
Sagar Prasad <sagar.11704088@gmail.com>
2024-06-15 17:46:03 +0530

    Multilingual feature added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,6 @@ import "./App.css";
 import { useState } from "react";
 import Student from "./Pages/Student";
 import Admin from "./Pages/Admin";
-import Auth from "./components/login.jsx";
-import About from "./components/About.jsx";
-import Footer from "./components/Footer.jsx";
 import { Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Admin/components/Sidebar";
 import Login from "./components/Admin/Login";
@@ -20,7 +17,7 @@ function App() {
         {roles === "student" && <Route path="/" element={<Student />} />}
 
         {roles === "donor" && <Route path="/" element={<Donar />} />}
-        
+
         {roles === "admin" && isLoggedIn ? (
           <Route path="admin/*" element={<Sidebar />}>
             <Route path="dashboard/*" element={<Admin />} />
@@ -29,10 +26,6 @@ function App() {
           <Route path="/" element={<Login />} />
         )}
       </Routes>
-
-      <Auth />      
-      <About />
-      <Footer />
     </>
   );
 }
