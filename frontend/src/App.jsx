59cc6b211cacb6c1e2ffcad723c48b7a1a2a098b
Sagar Prasad <sagar.11704088@gmail.com>
2024-06-17 14:03:30 +0530

    code formatted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,32 @@
-
 import "./App.css";
 import { useState } from "react";
 import Student from "./Pages/Student";
 import Admin from "./Pages/Admin";
-
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Admin/Home.jsx";
-import Login from "./components/Admin/Login.jsx";
 import PaymentForm from "./components/PaymentForm.jsx";
 import Sidebar from "./components/Admin/components/Sidebar";
-
 import Donar from "./Pages/Donar";
+import HomePage from "./components/Home.jsx";
+import Logins from "./components/Login/logins.jsx";
 
-
-  
 const App = () => {
-  const [roles, setRoles] = useState("donor");
-
+  const [roles, setRoles] = useState("student");
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
     <>
-
-    
-      <Auth />
-      <h1>Home Page</h1>
+      {/* <Auth />
+      <h1>Home Page</h1> */}
       <PaymentForm></PaymentForm>
-      <CreateProject />
-      <ProjectList projects={projects} />
+      {/* <CreateProject />
+      <ProjectList projects={projects} /> */}
 
       <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Logins />} />
-        {roles === "student" && <Route path="/" element={<Student />} />}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Logins />} />
+        {roles === "student" && <Route path="/student" element={<Student />} />}
 
         {roles === "donor" && <Route path="/" element={<Donar />} />}
-
 
         {roles === "donor" && <Route path="/donor/*" element={<Donar />} />}
 
@@ -46,14 +38,8 @@ const App = () => {
           <Route path="/" element={<Logins />} />
         )}
       </Routes>
-
-
-      {/* <Auth />      
-      <About />
-      <Footer /> */}
-
     </>
   );
-}
+};
 
 export default App;
